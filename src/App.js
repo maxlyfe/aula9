8258fb8b58052bfab009aea0ce21f84c286fbf0d
MaxLyFe <85650989+maxlyfe@users.noreply.github.com>
2021-08-26 20:59:28 -0300

    Aula09

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState , useEffect} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    
+    function Home(){
+        return <h1>Titulo React</h1>
+      }
+    
+    function Texto(props){
+        return <p>{props.texto}</p>
+      }
+    
+    function Paragrafos(){
+        return(
+          <div>
+            <Texto texto='Clica para aumentar'/>
+          </div>
+        );
+      }
+    function Botao(props){
+        const [botao, setBotao] = useState(0);
+      
+        useEffect(() => {
+          document.title = botao;
+        },[botao])
+        
+        return(
+          <div>
+            <p>linha 1 {botao}</p>
+            <button onClick={() => setBotao(botao+1)}>{props.nome}</button>
+          </div>
+        );
+    }
+    return(
+        <div>
+        <Home />
+        <Texto/>
+        <Paragrafos/>
+        <Botao nome='aumentar'/>
+        </div>
+    )
 }
 
-export default App;
+
